f2393e33a99c9c13e4e65b788062c9164ed354c0
Aleksandr Svinin <79195264446@ya.ru>
2025-06-17 15:34:32 +0300

    Complete task 2

--- cache.go
+++ cache.go
@@ -1,19 +1,50 @@
 package cache
 
-type Cache map[string]interface{}
+import (
+	"errors"
+	"sync"
+	"time"
+)
 
-func New() Cache {
-	return make(Cache)
+type cacheEntry struct {
+	value interface{}
+	ttl   time.Time
+}
+type Cache struct {
+	data map[string]cacheEntry
+	mu   sync.RWMutex
 }
 
-func (c Cache) Set(key string, value interface{}) {
-	c[key] = value
+func New() *Cache {
+	return &Cache{
+		data: make(map[string]cacheEntry),
+	}
 }
 
-func (c Cache) Get(key string) interface{} {
-	return c[key]
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
+	c.mu.Lock()
+	c.data[key] = cacheEntry{
+		value: value,
+		ttl:   time.Now().Add(ttl),
+	}
+	c.mu.Unlock()
 }
 
-func (c Cache) Delete(key string) {
-	delete(c, key)
+func (c *Cache) Get(key string) (interface{}, error) {
+	if entry, ok := c.data[key]; ok {
+		if time.Now().After(entry.ttl) {
+			c.Delete(key)
+			return nil, errors.New("ttl expired")
+		}
+		c.mu.RLock()
+		defer c.mu.RUnlock()
+		return entry.value, nil
+	}
+	return nil, errors.New("key not found")
+}
+
+func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	delete(c.data, key)
+	c.mu.Unlock()
 }
